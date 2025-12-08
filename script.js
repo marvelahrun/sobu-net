5dc749966b67c476de9ad8f19485be81ca247109
marvelahrun <realmarvelbrilliant@gmail.com>
2025-12-08 15:45:20 +0700

    Update Theme and Background

--- script.js
+++ script.js
@@ -4,3 +4,25 @@ document.querySelectorAll(".social-btn").forEach(btn => {
         console.log("Hovered:", btn.textContent);
     });
 });
+
+
+const switchBtn = document.getElementById("mode-switch");
+
+switchBtn.addEventListener("click", () => {
+    document.body.classList.toggle("dark-mode");
+
+    // Save preference
+    if (document.body.classList.contains("dark-mode")) {
+        localStorage.setItem("theme", "dark");
+        switchBtn.textContent = "Switch Tema (Experimental)";
+    } else {
+        localStorage.setItem("theme", "light");
+        switchBtn.textContent = "Switch Tema (Experimental)";
+    }
+});
+
+// Load preference on page load
+if (localStorage.getItem("theme") === "dark") {
+    document.body.classList.add("dark-mode");
+    switchBtn.textContent = "Tema";
+}
